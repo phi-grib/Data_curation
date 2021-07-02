eb843f5cef39ca10dbdd5357d91d02ca1560a197
Eric March <eric.march91@gmail.com>
2021-07-02 12:45:28 +0200

    minor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='data_curation',
+    name='data-curation',
     version='2.0',
     description='Data curation package. Optimized for pandas Dataframes',
     license='GNU',
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     # If any package contains *.txt or *.rst files, include them:
     # package_data={'': ['*.yaml', '*.yml']},
-    package_data={'data_curation': ['config.yaml','children/*.yaml']},
+    package_data={'curate': ['config.yaml','children/*.yaml']},
     entry_points={
        'console_scripts': ['datacur=curate.curate_src:main'],
     }
